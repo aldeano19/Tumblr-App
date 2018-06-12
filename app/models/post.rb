be3d2fc40e0db825a1c3eed6b57bfb8b8101c302
dibu2018 <erieljr1@gmail.com>
2018-06-12 05:23:24 -0400

    Added comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy # Destroys related comments when this post is deleted
+
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :posts
+  resources :posts do
+    # Embeds the routes for comments in posts. Ex: instead of GET /comments/:id it would be GET /posts/:post_id/comments/:id
+    resources :comments
+  end
 
   root "posts#index"
 
